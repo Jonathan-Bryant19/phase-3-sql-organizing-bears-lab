e10db86e33897fde62dc8987d13b7a4933af1f8e
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-26 20:40:59 -0800

    commit tree dcc6112dde072d8b8ce0a5521a36ab65266ba18f

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "Brown", "Chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 12, "M", "Black", "Laser Eyes", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 15, "F", "Brown", "Chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 19, "F", "Golden", "Not Chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 11, "M", "Brown", "Chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 7, "M", "Polar", "Watch Your Back", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 4, "F", "Brown", "Chill", 1);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES (9, "M", "Brown", "Chill", 1);
